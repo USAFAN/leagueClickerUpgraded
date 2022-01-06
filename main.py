4cc416f846db80f9d43a34a4494076e1ab36f044
etinator101@gmail.com <etinator101@gmail.com>
2022-01-06 17:46:15 -0600

    gg

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 #find picture on screen and click it
 import pyautogui
 
-
-onAcceptScreen = False
+import time
+accepting = True
 banned = False
+onAcceptScreen = True
+champ = "irelia"
+enemyBan = "teemo"
 
 while True:
     lockin = pyautogui.locateOnScreen('acceptpng.png',confidence=0.6,grayscale=True)
-    banSearch= pyautogui.locateOnScreen('banSearch.png',confidence=0.6,grayscale=True)
-    championPng = pyautogui.locateOnScreen('championPng.png',confidence=0.6,grayscale=True)
+    banSearch= pyautogui.locateOnScreen('banSearch.png',confidence=0.6)
+    #championPng = pyautogui.locateOnScreen('championPng.png',confidence=0.6,grayscale=True)
+    champSearch = pyautogui.locateOnScreen('champSearch.png',confidence=0.6)
+
     
     print(lockin)
     print(banSearch)
-    if lockin != None:
+    if lockin != None and accepting:
         print("found lockin")
         pyautogui.click(lockin)
         onAcceptScreen = True
-    if banSearch != None:
+    if champSearch != None:
         print("found banSearch")
         onAcceptScreen = False
-        pyautogui.click(banSearch)
+        pyautogui.click(champSearch)
         #type "irelia" 
-        pyautogui.typewrite("irelia")
+        pyautogui.typewrite(champ)
+        time.sleep(3)
+
+        #banned = True
+    if banSearch != None:
+        print("found banSearch")
+        pyautogui.click(champSearch)
+        #type "irelia" 
+        pyautogui.typewrite(enemyBan)
+        time.sleep(3)
         banned = True
     #reset varibles if we come back to the accept screen
     if onAcceptScreen:
